70dc1b64ebc6f3dad5da061a8a890a2f20a4393e
atompsv <tomtomtom.t953@gmail.com>
2024-06-12 16:26:33 +0700

    Fix dns

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
-	"github.com/jvongxay0308/database-go"
 	"github.com/labstack/echo/v4"
 	_ "github.com/lib/pq"
 )
@@ -15,21 +14,44 @@ var (
 	PGUSER     = os.Getenv("PGUSER")
 	PGPASSWORD = os.Getenv("PGPASSWORD")
 	PGHOST     = os.Getenv("PGHOST")
-	// PGPORT     = os.Getenv("PGPORT")
+	PGPORT     = os.Getenv("PGPORT")
 	PGDATABASE = os.Getenv("PGDATABASE")
 )
 
 func main() {
+
+	// fmt.Println("Starting the application...")
+	// // Connect to the database.
+	// db, err := connectTCPSocket()
+	// if err != nil {
+	// 	log.Fatalf("connectTCPSocket: %v", err)
+	// }
+	// defer db.Close()
+
 	// fmt.Println("Connected to the database.")
-	int, _ := os.Hostname()
-	db, err := database.Open("pgx", fmt.Sprintf("postgres://%s@%s:%s/%s?sslmode=disable&connect_timeout=%d",
-		PGUSER, PGPASSWORD, PGHOST, PGDATABASE, 10), int)
+	db, err := sql.Open("postgres", fmt.Sprintf("host=/cloudsql/%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		PGHOST, PGPORT, PGUSER, PGPASSWORD, PGDATABASE))
 	if err != nil {
 		log.Fatalf("failed to open database: %v", err)
 	}
 	defer db.Close()
 
-	ctx := context.Background()
+	// ctx := context.Background()
+
+	query, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		log.Fatalf("db.Query: %v", err)
+	}
+	defer query.Close()
+
+	users := make([]User, 0)
+	for query.Next() {
+		var user User
+		if err := query.Scan(&user.ID, &user.Name); err != nil {
+			log.Fatalf("query.Scan: %v", err)
+		}
+		users = append(users, user)
+	}
 
 	app := echo.New()
 	app.GET("/", func(c echo.Context) error {
@@ -37,21 +59,6 @@ func main() {
 	})
 
 	app.GET("/users", func(c echo.Context) error {
-		query, err := db.Query(ctx, "SELECT * FROM users")
-		if err != nil {
-			log.Fatalf("db.Query: %v", err)
-		}
-		defer query.Close()
-
-		users := make([]User, 0)
-		for query.Next() {
-			var user User
-			if err := query.Scan(&user.ID, &user.Name); err != nil {
-				log.Fatalf("query.Scan: %v", err)
-			}
-			users = append(users, user)
-		}
-
 		return c.JSON(200, users)
 	})
 
@@ -62,4 +69,38 @@ func main() {
 type User struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
+}
+
+// connectTCPSocket initializes a TCP connection pool for a Cloud SQL
+// instance of MySQL.
+func connectTCPSocket() (*sql.DB, error) {
+	mustGetenv := func(k string) string {
+		v := os.Getenv(k)
+		if v == "" {
+			log.Fatalf("Fatal Error in connect_tcp.go: %s environment variable not set.", k)
+		}
+		return v
+	}
+	// Note: Saving credentials in environment variables is convenient, but not
+	// secure - consider a more secure solution such as
+	// Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+	// keep secrets safe.
+	var (
+		dbUser    = mustGetenv("DB_USER")       // e.g. 'my-db-user'
+		dbPwd     = mustGetenv("DB_PASS")       // e.g. 'my-db-password'
+		dbName    = mustGetenv("DB_NAME")       // e.g. 'my-database'
+		dbPort    = mustGetenv("DB_PORT")       // e.g. '3306'
+		dbTCPHost = mustGetenv("INSTANCE_HOST") // e.g. '127.0.0.1' ('172.17.0.1' if deployed to GAE Flex)
+	)
+
+	// dbPool is the pool of database connections.
+	dbPool, err := sql.Open("postgres", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?sslmode=disable",
+		dbUser, dbPwd, dbTCPHost, dbPort, dbName))
+	if err != nil {
+		return nil, fmt.Errorf("sql.Open: %w", err)
+	}
+
+	// ...
+
+	return dbPool, nil
 }
